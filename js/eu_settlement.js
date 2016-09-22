e4958783498eef1dde058cc7c39309a3a415377c
Rosie Bell <mrsrosiebell@gmail.com>
2016-09-22 14:32:57 +0800

    stamen loading, slider displays correct years. No markers loading.

--- js/eu_settlement.js
+++ js/eu_settlement.js
@@ -11,9 +11,18 @@ var yearArray = [0, 1788,	1791,	1798,	1803,	1804,	1806,	1807,	1808,	1812,	1813,	
   1882,	1884,	1885,	1886,	1887,	1888,	1890,	1891,	1892,	1893,	1896,	1904,	1911,	1913,
   1915,	1916,	1917,	1920,	1923,	1924,	1926,	1961,	1965,	1967,	1968,	1980,	1981,	1982,	1987]
 
+// After the loop, each feature where the year is less than year selected in the slider, will be added to the array in this object
+var chosenYearObject = {
+  "type": "FeatureCollection",
+  "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  "features": [
+  ]
+};
+
 // =====================================
 // Functions
 // =====================================
+
 // For map: function to initialise the map
 function initmap() {
   var stamen = new L.StamenTileLayer("watercolor");
@@ -25,11 +34,43 @@ function initmap() {
 }
 
 //For slider: this function updates the text value when you move the slider so year is displayed under the slider
-function outputUpdate(vol) {
-  document.querySelector('#volume').textContent = vol;
+function updateYear(year) {
+  document.querySelector('#displayedYear').textContent = year;
 }
+
+function pushSelectedYears (year) {
+  for (var i = 0; i < locationObject.features.length; i++) {
+  // assigns i to a new variable feature (for convenience?)
+  var feature = locationObject.features[i];
+
+  //pushes each feature where year is less than or equal to (slider value) to a new object
+  if (feature.properties.year <= year) {
+    chosenYearObject.features.push(feature);
+  }
+  };
+  return chosenYearObject;
+}
+
 
 // =====================================
 // Calling the functions
 // =====================================
 initmap();
+
+// L.geoJson(locationObject).addTo(map);
+
+document.getElementById('yearSlider').addEventListener('change', function() {
+  //2. clears any features from previous years in array
+  chosenYearObject.features = [];
+  sliderIndex = this.value;
+  //3. gets the year from the yearArray
+  year = yearArray[sliderIndex];
+  // Finds all values <= slider year and pushes them to a new object
+  pushSelectedYears(year);
+  ///test condition
+  /// console.log(year);
+  console.log(chosenYearObject);
+  // not working - investigate tomorrow
+  L.geoJson(chosenYearObject).addTo(map);
+
+});
--- js/locations.js
+++ js/locations.js
@@ -1,4 +1,4 @@
-var locations = {
+var locationObject = {
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 
