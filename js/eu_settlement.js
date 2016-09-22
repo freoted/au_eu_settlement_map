7401f0c63c43aba42cdd6c38886e7f6cfadf9b5d
Rosie Bell <mrsrosiebell@gmail.com>
2016-09-22 21:19:36 +0800

    initialises with 1788. Removes 1788 markers when slider moves. Doesn't add any new markers

--- js/eu_settlement.js
+++ js/eu_settlement.js
@@ -14,10 +14,14 @@ var yearArray = [0, 1788,	1791,	1798,	1803,	1804,	1806,	1807,	1808,	1812,	1813,	
   1915,	1916,	1917,	1920,	1923,	1924,	1926,	1961,	1965,	1967,	1968,	1980,	1981,	1982,	1987]
 
 // After the loop, each feature where the year is less than year selected in the slider, will be added to the array in this object
+// initialise with 1788 features
 var chosenYearObject = {
   "type": "FeatureCollection",
   "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
   "features": [
+    { "type": "Feature", "properties": { "year": 1788, "town": "Sydney", "state": "New South Wales", "approx_yr": "N" }, "geometry": { "type": "Point", "coordinates": [ 151.2092955, -33.8688197 ] } },
+    { "type": "Feature", "properties": { "year": 1788, "town": "Parramatta", "state": "New South Wales", "approx_yr": "N" }, "geometry": { "type": "Point", "coordinates": [ 151.001111, -33.815 ] } },
+    { "type": "Feature", "properties": { "year": 1788, "town": "Kingston", "state": "Norfolk Island", "approx_yr": "N" }, "geometry": { "type": "Point", "coordinates": [ 167.959588, -29.0563937 ] } }
   ]
 };
 
@@ -42,10 +46,10 @@ function updateYear(year) {
 }
 
 function pushSelectedYears (year) {
+  chosenYearObject.features = [];
   for (var i = 0; i < locationObject.features.length; i++) {
   // assigns i to a new variable feature (for convenience?)
   var feature = locationObject.features[i];
-
   //pushes each feature where year is less than or equal to (slider value) to a new object
   if (feature.properties.year <= year) {
     chosenYearObject.features.push(feature);
@@ -64,21 +68,18 @@ initmap();
 
 // var year1908 = new L.geoJson(locationObject).addTo(map);
 
+var currentMarkers = L.geoJson(chosenYearObject).addTo(map);
 
 
 
 document.getElementById('yearSlider').addEventListener('change', function() {
   //2. clears any features from previous years in array
-  chosenYearObject.features = [];
+  map.removeLayer(currentMarkers);
   sliderIndex = this.value;
   //3. gets the year from the yearArray
   year = yearArray[sliderIndex];
   // Finds all values <= slider year and pushes them to a new object
   pushSelectedYears(year);
-  ///test condition
-  /// console.log(year);
-  // console.log(chosenYearObject);
-  // not working - investigate tomorrow
-  L.geoJson(chosenYearObject).addTo(map);
+  // var currentMarkers = L.geoJson(chosenYearObject).addTo(map);
 
 });
